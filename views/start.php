d1b7093911cc1a10114a0350bf5132f4dae739bf
melle01 <mellecedervall@gmail.com>
2019-10-13 16:41:25 +0200

    second

---
+++ views/start.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Visma taxi</title>
+
+	
+	<link rel="stylesheet" type="text/css" href="css/visma.css">
+	<link rel="stylesheet" type="text/css" href="css/orders.css">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,600&display=swap" rel="stylesheet">
+
+</head>
+<body>
+
+<div class="nav">
+
+ 	<div class="headline"><div><img src="img/logo.svg" style="width:128px;"></div><div style="padding-left: 10px; font-size: 28px;">Taxi
+ 	</div></div>
+
+ 	<div class="links"><a href="registrera.php">Registrera dig</a></div>
+
+ </div>
+
+ <div class="container">
+
+
+ 
+ <div class="loginbox">
+ <h1 style="padding-bottom: 25px;">Logga in </h1>
+ <form action="login.php" method="POST">
+
+ <input type="text" name="user" placeholder="Användarnamn">
+ <input type="password" name="password" placeholder="Lösenord">
+ 
+
+</form>
+
+<a href="login.php">  
+
+ <button>Logga in</button>
+
+</a>
+	
+ </div>
+
+ <img class="firstpageimg" style="height: 550px; position: absolute; top: 200px; left: 10px;"  src="img/Businessman-Transparent.png">
+
+ </div>
+
+ </body>
+
+</html>
